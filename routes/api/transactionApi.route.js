4ec85b3db8af413a1a5da3ceadd8cfdc4c1b1a22
Gia-Kiet-99 <kietdg99@gmail.com>
2021-05-17 15:51:59 +0700

    Update transaction detail page and transaction api route: enable to access unconfirm transaction page

--- routes/api/transactionApi.route.js
+++ routes/api/transactionApi.route.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const blockModel = require('../../model/blockchain');
+const txPoolModel = require('../../model/transaction-pool');
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
   const id = req.params.id;
 
   let isConfirmed = false;
   let { transaction, blockIndex } = blockModel.findTransaction(id);
+  const unspentTxOutsOfTransaction = [];
 
   let allTransaction = [];
   if (transaction) {
@@ -16,23 +18,31 @@ router.get('/:id', (req, res) => {
     for (let i = 0; i < blockIndex; i++) {
       allTransaction.push(...(blockchain[i].data));
     }
+    const txIns = transaction.txIns;
+
+    for (const txIn of txIns) {
+      const referenceTx = allTransaction.find(tx => tx.id === txIn.txOutId);
+      if (referenceTx) {
+        unspentTxOutsOfTransaction.push({
+          txOutId: txIn.txOutId,
+          txOutIndex: txIn.txOutIndex,
+          address: referenceTx.txOuts[txIn.txOutIndex].address,
+          amount: referenceTx.txOuts[txIn.txOutIndex].amount
+        })
+      }
+    }
   } else {
     transaction = txPoolModel.getTransactionPool().find(tx => tx.id === id);
-    allTransaction = blockModel.getBlockChain().map(block => block.data).flat();
-  }
-  
-  const txIns = transaction.txIns;
-  const unspentTxOutsOfTransaction = [];
-
-  for (const txIn of txIns) {
-    const referenceTx = allTransaction.find(tx => tx.id === txIn.txOutId);
-    if (referenceTx) {
-      unspentTxOutsOfTransaction.push({
-        txOutId: txIn.txOutId,
-        txOutIndex: txIn.txOutIndex,
-        address: referenceTx.txOuts[txIn.txOutIndex].address,
-        amount: referenceTx.txOuts[txIn.txOutIndex].amount
-      })
+    if (!transaction) {
+      next();
+    }
+    const unspentTxOuts = blockModel.getUnspentTxOuts();
+    let correspondingUTXO;
+    for (const txIn of transaction.txIns) {
+      correspondingUTXO = unspentTxOuts.find(uTxO => uTxO.txOutId === txIn.txOutId && uTxO.txOutIndex === txIn.txOutIndex);
+      if (correspondingUTXO) {
+        unspentTxOutsOfTransaction.push(correspondingUTXO);
+      }
     }
   }
 
